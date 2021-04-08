55e89a3b852ace501b4daf30f7700ca93ade9c72
Dhilip RK <dhilipram@gmail.com>
2021-04-08 18:22:23 +0200

    Update application.py
    added products url

--- application.py
+++ application.py
@@ -14,23 +14,23 @@ from gremlin_python.process.graph_traversal import __
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
 from flask import Flask
     
-def invoke_gremlin_connection():
-    graph = Graph()
-    connection = DriverRemoteConnection('ws://10.1.0.4:8182/gremlin', 'g')
-    # The connection should be closed on shut down to close open connections with connection.close()
-    g = graph.traversal().withRemote(connection)
-    # Reuse 'g' across the application
-    gcnt1 = g.V().count()
-    return str(gcnt1)
+graph = Graph()
+connection = DriverRemoteConnection('ws://10.1.0.4:8182/gremlin', 'g')
+# The connection should be closed on shut down to close open connections with connection.close()
+g = graph.traversal().withRemote(connection)
+# Reuse 'g' across the application
 
 app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return invoke_gremlin_connection()
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
+
+@app.route('/product')
+def get_product():
+    #fetch product vertex with Product Id and Product Name properties. limit rows to 5
+    products = g.V().hasLabel('Product').limit(5).valueMap('productID','productName').toList()
+    return str(products)
+    
 
