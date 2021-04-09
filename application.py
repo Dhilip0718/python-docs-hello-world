f80a6eb709054d17a33981788c6ef1ba42955eec
C72635 <dhilip.rk@nl.abnamro.com>
2021-04-09 13:10:37 +0200

    added new imports from DB

--- application.py
+++ application.py
@@ -1,9 +1,29 @@
 # Gremlin imports
+# from gremlin_python import statics
+# from gremlin_python.structure.graph import Graph
+# from gremlin_python.process.graph_traversal import __
+# from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+
 from gremlin_python import statics
-from gremlin_python.structure.graph import Graph
+from gremlin_python.process.anonymous_traversal import traversal
 from gremlin_python.process.graph_traversal import __
+from gremlin_python.process.strategies import *
+from gremlin_python.structure.graph import Graph
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+from gremlin_python.process.traversal import T
+from gremlin_python.process.traversal import Order
+from gremlin_python.process.traversal import Cardinality
+from gremlin_python.process.traversal import Column
+from gremlin_python.process.traversal import Direction
+from gremlin_python.process.traversal import Operator
+from gremlin_python.process.traversal import P
+from gremlin_python.process.traversal import Pop
+from gremlin_python.process.traversal import Scope
+from gremlin_python.process.traversal import Barrier
+from gremlin_python.process.traversal import Bindings
+from gremlin_python.process.traversal import WithOptions
 from flask import Flask
+
 import json
     
 graph = Graph()
